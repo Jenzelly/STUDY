a761f28af74d05af2e2c313abd06926349c7a9db
Jenzelly <229729343+Jenzelly@users.noreply.github.com>
2026-09-09 10:27:45 +0900

    Q4.10 임의의 숫자를 균등한 확률로 생성하기

---
+++ CPP/ch04_기본자료형/ex4_10_uniform_random.cpp
@@ -0,0 +1,20 @@
+﻿/*
+문제 4.10
+
+임의의 숫자를 균등한 확률로 생성하기
+*/
+
+int UniformRandom (int lower_bound, int upper_bound)
+{
+    int number_of_outcomes = upper_bound - lower_bound +1, result;
+    do
+    {
+        result = 0;
+        for( int i = 0; (1<<i) < number_of_outcomes; ++i)
+        {
+            //ZeroOneRandom()은 임의의 숫자를 생성하는 함수이다.
+            result = (result << 1) | ZeroOneRandom();
+        }
+    } while (result >= number_of_outcomes);
+    return result + lower_bound;
+}
