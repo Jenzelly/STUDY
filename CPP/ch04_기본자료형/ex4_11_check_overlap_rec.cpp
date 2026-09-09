67974ef575682ab920d9738e7f3bcdb7d8a22531
Jenzelly <229729343+Jenzelly@users.noreply.github.com>
2026-09-09 10:27:46 +0900

    Q4.11 사각형이 겹치는 지 확인하기

---
+++ CPP/ch04_기본자료형/ex4_11_check_overlap_rec.cpp
@@ -0,0 +1,30 @@
+﻿/*
+문제 4.11
+사각형이 겹치는지 확인하기
+*/
+
+#include <algorithm>
+using namespace std;
+
+struct Rect
+{
+    int x, y, width, height;
+};
+
+bool IsIntersect(const Rect& r1, const Rect& r2)
+{
+    return r1.x <= r2.x + r2.width && r1.x + r1.width >= r2.x &&
+            r1.y <= r2.y + r2.height && r1.y + r1.height >= r2.y;
+}
+
+Rect IntersectRectangle(const Rect& r1, const Rect& r2)
+{
+    if ( !IsIntersect (r1, r2))
+    {
+        return {0, 0, -1, -1};
+    }
+
+    return {max(r1.x, r2.x), max(r1.y, r2.y),
+            min(r1.x + r1.width, r2.x + r2.width) - max(r1.x, r2.x),
+            min(r1.y + r1.height, r2.y + r2.height) - max(r1.y, r2.y)};
+}
