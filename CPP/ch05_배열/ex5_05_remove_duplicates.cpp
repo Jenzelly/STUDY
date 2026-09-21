df07ab464a311589a0b1cd7acb518fe42efec6cd
Jenzelly <229729343+Jenzelly@users.noreply.github.com>
2026-09-21 10:45:57 +0900

    Q5.5 정렬된 배열에서 중복 제거하기 Q5.6 주식 한 번 사고팔기

---
+++ CPP/ch05_배열/ex5_05_remove_duplicates.cpp
@@ -0,0 +1,26 @@
+﻿/*
+문제 5.5
+정렬된 배열에서 중복 제거하기
+*/
+
+#include <vector>
+using namespace std;
+
+int DeleteDuplicates(vector<int>* A_ptr)
+{
+    vector<int>& A = *A_ptr;
+    if (empty(A))
+    {
+        return 0;
+    }
+
+    int write_index = 1;
+    for (int i = 1; i < size(A); ++i)
+    {
+        if (A[write_index -1] != A[i])
+        {
+            A[write_index++] = A[i];
+        }
+    }
+    return write_index;
+}
---
+++ CPP/ch05_배열/ex5_06_buy&sell_stock.cpp
@@ -0,0 +1,22 @@
+﻿/*
+문제 5.6
+주식 한 번 사고팔기
+*/
+
+#include <vector>
+#include <algorithm>
+#include <limits>
+
+using namespace std;
+
+double BuyAndSellStockOnce ( const vector<double>& prices)
+{
+    double min_price_so_far = numeric_limits<double>::infinity(), max_profit = 0;
+    for (double price : prices)
+    {
+        double max_profit_sell_today = price - min_price_so_far;
+        max_profit = max(max_profit, max_profit_sell_today);
+        min_price_so_far = min(min_price_so_far, price);
+    }
+    return max_profit;
+}
